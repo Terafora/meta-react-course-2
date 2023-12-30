5881005c90e3b1c88d3c66b8ca65cc5175f7f6e9
Charlotte Stone <charlie.stone649@gmail.com>
2023-12-30 20:45:20 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return (
+    <h1>皆さんこんにちは！</h1>
+  )
+}
+
+function App() {
+  return (
+    <div clasName="App">
+      <Header />
+      <p>お元気ですか？</p>
+    ></div>
+  )
+}
+
+export default App;
